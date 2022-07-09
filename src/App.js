6928129098e53b0318aed1f22ce910d7a4d08434
higashimoto-kanami <>
2022-07-09 16:19:03 +0900

    とらゼミ＃07 prveState

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'; //reactから関数をimportする必要がある
 import './App.css';
 import TextInput from './compornents/TextInput.jsx';
+import Counter from './compornents/Counter.jsx';
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <p>名前：<TextInput/></p>
       <p>メール:<TextInput/></p>
+      <Counter/>
     </div>
   );
 };
---
+++ src/compornents/Counter.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from 'react';
+
+const Counter = () => {
+    const [count, setCount] = useState(0)
+
+    const countUp = () => {
+        setCount(prveState => prveState + 1)
+    }
+    const countDown = () => {
+        setCount(prveState => prveState - 1)
+    }
+
+    return (
+        <div>
+            <p>カウント{count}</p>
+            <button onClick={countUp}>Up</button>
+            <button onClick={countDown}>Down</button>
+        </div>
+    )
+}
+
+export default Counter;
