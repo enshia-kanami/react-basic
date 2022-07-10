e8742e988ab6c3bc1bdd78051b4c9a464dc74876
higashimoto-kanami <>
2022-07-10 19:43:32 +0900

    Search.jsxディレクトリ削除

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import TextInput from './compornents/TextInput.jsx';
 import Counter from './compornents/Counter.jsx';
 import ToggleButton from './compornents/ToggleButton.jsx';
-import Search from './compornents/Search.jsx';
 import { getByTitle } from '@testing-library/react';
 import Recipe from './compornents/Recipe';
 
@@ -11,21 +10,35 @@ function App() {
 
     const APP_ID = '3daae338'
     const APP_KEY = '116bbdfa155d444083161b48ebb7cbd4'
-    
-    const [query, setQuery] = useState("banana");
-    const [recipes, setRecipes] = useState([]);
+
+    const [search, setSearch] = useState('');
+    const [query, setQuery] = useState("banana");//キーワードの部分を指定するstate
+    const [recipes, setRecipes] = useState([]);//APIから情報を取得してきた情報を入れておくためのstate
+
+    const updateSearch = e => { //更新用
+      setSearch(e.target.value);
+    }
 
     //recipeAPIからrecipeを取ってくる為の関数
     const getRecipes = async () => {
-        const response = await fetch(`https://api.edamam.com/search?q="banana"&app_id=${APP_ID}&app_key=${APP_KEY}`)
+        const response = await fetch(`https://api.edamam.com/search?q="${query}"&app_id=${APP_ID}&app_key=${APP_KEY}`)
         const data = await response.json();
         setRecipes(data.hits);
         console.log(data.hits);
     }
 
+    const getSearch = e => {
+      e.preventDefault();
+      setQuery(search);
+      setSearch('');
+    }
+    
+    useEffect( () => {
+      console.log(search)
+    },[search]) //変更されるたびに呼ばれる
     useEffect( () => {
         getRecipes();
-    }, [])  //初回レンダリングのときに呼び出す
+    }, [query])  //初回レンダリングのときに呼び出す
 
   return(
     <div>
@@ -33,6 +46,10 @@ function App() {
       <p>メール:<TextInput/></p>
       <Counter/>
       <ToggleButton/> */}
+      <form onSubmit={getSearch}>
+        <input type={'text'} value={search} onChange={updateSearch}/>
+        <button type={'submit'}>検索</button>
+      </form>
       {recipes.map(recipe => ( 
       <Recipe
          key={recipe.recipe.lable}
